72f3157e8a6b6b49e2257074e05eb9ca66cf52c8
RobbiiB <nexufire1ais@gmail.com>
2026-03-13 12:11:09 +0100

    day_2_part_1_fin
    worked on day 2

--- day_2.py
+++ day_2.py
@@ -6,25 +6,34 @@ with open("data2.txt", "r") as file:
     data = rawdata.split(",")
     file.close()
 
-
+# part 1
 def get_bins(data_line):
     start, stop = data_line.split("-")
     len = start.__len__()
     stop_len = stop.__len__()
     bins = []
-    if len%2==1:
-        len+=1
-    while len<=stop_len:
-        
-        s = max(int(start[:len//2]) +(1 if int(start[:len//2])<int(start[len//2:]) else 0), 10**(len//2 - 1))
-        
-        if len == stop_len:
-            e = int(stop[:stop_len//2])-(1 if int(stop[:stop_len//2])>int(stop[stop_len//2:]) else 0)
-        else:
-            e = 10**(len//2)-1
-        bin = (s,e,len//2)
-        bins.append(bin)
-        len+=2    
+    if len==stop_len and len%2==1:
+        pass
+    else:
+        while len<=stop_len:
+            if len%2==1:
+                len+=1
+                s = 10**(len//2 - 1)
+            else:
+                s = max(int(start[:len//2]) +(1 if int(start[:len//2])<int(start[len//2:]) else 0), 10**(len//2 - 1))
+                print(10**(len//2 - 1))
+                
+            if len == stop_len:
+                e = int(stop[:stop_len//2])-(1 if int(stop[:stop_len//2])>int(stop[stop_len//2:]) else 0)
+            else:
+                e = 10**(len//2)-1
+                
+            if s>e:
+                bin = (0,0,len//2)
+            else:
+                bin = (s,e,len//2)
+            bins.append(bin)
+            len+=2    
         
     return bins
 
@@ -37,8 +46,11 @@ for line in data:
     for bin in bins:
         
         s,e,len = bin
-        invalid_id_sum+=(1+10**len)*(s+e-1)*(e-s+1)//2
+        invalid_id_sum+=(1+10**len)*(e**2+e-s**2+s)/2
 t_2 = time()
 
 print(invalid_id_sum)
 print(f"time taken {int((t_2-t_1)*1000000)/1000} ms")
+
+# part 2
+
